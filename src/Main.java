0df24f3bc7c3eb56fb8abf8c7ee3d4fb67b69018
zb63 <spaweloszek63@gmail.com>
2020-03-04 15:18:26 +0100

    Modyfikacja wklejonego 01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("W drugim branchu; 3+3="+policz(3,3));
+    }
+
+    public static int policz(int x, int y){
+        return x+y;
     }
 }
