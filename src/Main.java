8c8306bd6b034336dca99291035edd8c65e3f6e7
zb63 <spaweloszek63@gmail.com>
2020-03-04 15:14:52 +0100

    Dodano funkcje policz()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("W pierwszym branchu; 2+3="+policz(2,3));
+    }
+
+    public static int policz(int x, int y){
+        return x+y;
     }
 }
